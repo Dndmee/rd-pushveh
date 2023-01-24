4f6a8f8c49fa710f1ef52b1bd75d80979e16d224
Dndmee <73814604+Dndmee@users.noreply.github.com>
2023-01-24 10:13:20 +0700

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version "bodacious"
+game "gta5"
+
+
+name "PushOver"
+description "A simple Standalone script to push cars"
+author "RD DEVELOPMENT TEAM"
+version "v1.0.0"
+
+
+client_script "config.lua"
+client_script "client.lua"
+
+server_script "version_check.lua"
